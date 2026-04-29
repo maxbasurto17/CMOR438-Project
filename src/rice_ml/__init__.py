df62e2f4104aa6a481ad56409b1202d3c54f2f29
suhasnarra <suhasnarra@gmail.com>
2026-04-28 21:14:51 -0500

    Fix adding measures directory to general package import.

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -13,4 +13,4 @@ from .preprocessing.standard_scaler import StandardScaler
 from .preprocessing.one_hot_encoder import OneHotEncoder
 from .unsupervised.k_means import KMeans
 from .measures.distances import *
-from measures.validation import *
+from .measures.validation import *
